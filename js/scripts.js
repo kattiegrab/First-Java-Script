0ea88f80f23d84b002cefd924ed97577827f8339
Kasia Grabowska <kattie.grab@gmail.com>
2017-04-20 12:04:14 +0200

    test-comment

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 // plik scripts.js
 
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
+/*
+	The code below
+	will open a welcome window.
+*/
+
+var name = prompt('Wpisz swoje imię'); //task: Wpisz swoje imię
+//alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
